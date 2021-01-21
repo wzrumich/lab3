1bbf234003ddb57826010807c3dae8ed4785d558
Zerui Wei <wezerui@umich.edu>
2021-01-21 09:47:07 -0500

    click should be fade out

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -28,6 +28,6 @@ function projectClick(e) {
     if (description.length == 0) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(".project-description").fadeOut();
     }
 } 
